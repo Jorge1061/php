d2a16de94f4e6a517cf2999a94d0c870bdafd38d
Jorge1061 <84825431+Jorge1061@users.noreply.github.com>
2021-07-20 16:00:18 -0600

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,7 +1,6 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
 $resultado = $_POST['valorCaja1'] + $_POST['valorCaja2']; 
 echo $resultado;
 ?>
