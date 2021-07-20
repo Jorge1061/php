00aab4e38f9361065923d6eab313915ab07c84cd
Jorge1061 <84825431+Jorge1061@users.noreply.github.com>
2021-07-20 16:40:33 -0600

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,1 +1,1 @@
-<?php header('Access-Control-Allow-Origin: *'); $resultado = $_GET['valorCaja1'] + $_GET['valorCaja2']; echo $resultado; ?>
+<?php header('Access-Control-Allow-Origin: *'); echo $_GET['valorCaja1'] + $_GET['valorCaja2']; ?>
