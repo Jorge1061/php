fe80dd22342a5524d7437b7d1cc731302204e727
Jorge1061 <84825431+Jorge1061@users.noreply.github.com>
2021-07-20 15:49:37 -0600

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,5 +1,5 @@
 <?php 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: index.html:1');
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
