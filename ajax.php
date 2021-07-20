6d783920f044a27c739adc3480638bcfe2a11579
Jorge1061 <84825431+Jorge1061@users.noreply.github.com>
2021-07-20 15:42:50 -0600

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,5 +1,5 @@
 <?php 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: file:///C:/Users/EstebanDur%C3%A1n(AuraQua/Downloads/Jquery/index.html');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 $resultado = $_POST['valorCaja1'] + $_POST['valorCaja2']; 
